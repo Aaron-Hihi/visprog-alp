8562b8b267b8196adf61ab1eb6d7490eb8777513
Aaron-Hihi <aadiputra@student.ciputra.ac.id>
2025-12-12 21:22:56 +0700

    Theme setting

--- app/src/main/java/com/aaron/walkcore/ui/theme/Type.kt
+++ app/src/main/java/com/aaron/walkcore/ui/theme/Type.kt
@@ -2,19 +2,78 @@ package com.aaron.walkcore.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.aaron.walkcore.R
 
 // Set of Material typography styles to start with
+
+val MontserratAlternatesFontFamily = FontFamily(
+    Font(R.font.montserrat_alternates_thin, FontWeight.Thin),
+    Font(R.font.montserrat_alternates_extralight, FontWeight.ExtraLight),
+    Font(R.font.montserrat_alternates_light, FontWeight.Light),
+    Font(R.font.montserrat_alternates_regular, FontWeight.Normal),
+    Font(R.font.montserrat_alternates_medium, FontWeight.Medium),
+    Font(R.font.montserrat_alternates_semibold, FontWeight.SemiBold),
+    Font(R.font.montserrat_alternates_bold, FontWeight.Bold),
+    Font(R.font.montserrat_alternates_extrabold, FontWeight.ExtraBold),
+    Font(R.font.montserrat_alternates_black, FontWeight.Black)
+)
+
+val GeoramaFontFamily = FontFamily(
+    Font(R.font.georama_variable)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineLarge = TextStyle(
+        fontFamily = MontserratAlternatesFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    headlineMedium = TextStyle(
+        fontFamily = MontserratAlternatesFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    headlineSmall = TextStyle(
+        fontFamily = MontserratAlternatesFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = GeoramaFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    bodyMedium = TextStyle(
+        fontFamily = GeoramaFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    bodySmall = TextStyle(
+        fontFamily = GeoramaFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
